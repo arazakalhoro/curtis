8ae6f53d01d2fb856c4c9f019b0dee2edaabd3eb
Kalhoro Ali Raza <arazakalhoro@gmail.com>
2016-02-14 22:04:10 -0800

    Re-Update main.js
    Replace main.js with this one

--- assets/js/main.js
+++ assets/js/main.js
@@ -300,9 +300,9 @@ audio
 email validation
 =================================================*/
   function _formValidation(email_address) {
-	  alert(email_address);
+	
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    alert(pattern.test(email_address))
+    
 	return pattern.test(email_address);
   }
 
@@ -320,8 +320,8 @@ subscribe form
   /* mailchimp */
   function _mailchimp() {
 
-    var $form = $('#subscribe-form');
-    var $subscribeEmail = $('#subscribe-email');
+    var $form = $('#subscribe_form');
+    var $subscribeEmail = $('#subscribe_email');
 
     $form.ajaxChimp({
       callback: _mailChimpStatus,
@@ -354,11 +354,11 @@ subscribe form
 
     var $form = $('#subscribe_form');
     var $subscribeEmail = $('#subscribe_email');
-    alert('in function');
+    
     $subscribeEmail.prop('type', 'text');
 
     
-      alert('in form .on function');
+      
       var subscribeEmailVal = $subscribeEmail.val();
       var $subscribeNotice = $('.subscribe-notice');
       var $submitButton = $form.find('button[type="submit"]');
@@ -373,7 +373,7 @@ subscribe form
         $('#subscribe-email').focus();
       }
       else {
-		   alert('in ajax ');
+		   
         $.ajax({
           type: 'POST',
           url: 'assets/php/subscribe.php',
@@ -387,7 +387,7 @@ subscribe form
             $submitButton.prop('disabled', false);
             $form[0].reset();
             $subscribeEmail.blur();
-            alert('in ajax success');
+            
           }
         });
       }
